bedb038fe0310b2ac16039d2c4830a6d1374f04b
Arielparicio28 <arielaparicio100@gmail.com>
2025-01-21 12:02:02 +0100

    Config tailwind responsive dimensions

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,14 +8,13 @@
     fontFamily: {
       primary: 'Arial',
     },
-    // screens: {
-    //   sm: '640px',
-    //   md: '768px',
-    //   lg: '1024px',
-    //   xl: '1200px',
-    //   xxl: '1600px',
-    //   xxxl: '1900px',
-    // },
+     screens: {
+      'md': '769px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1930px',
+     },
     extend: {
       colors: {
         primary: '#0E1112',
